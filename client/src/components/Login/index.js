c5cac001d2d6b71c11799cbe2e7bc7083ff81ca3
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-19 15:39:44 -0500

    Updates

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { useApolloClient } from "@apollo/react-hooks";
 import { Avatar, Button, TextField, FormControlLabel, Checkbox, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './useStyles';
 
 export default function Login() {
   const classes = useStyles();
+  const client = useApolloClient();
   return (
     <Container component="main" maxWidth="xs">
       <div className={classes.paper}>
@@ -15,7 +17,10 @@ export default function Login() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} onSubmit={e => { e.preventDefault() }}>
+        <form className={classes.form} onSubmit={e => {
+          e.preventDefault();
+          client.writeData({ formData: e })
+        }}>
           <TextField
             variant="outlined"
             margin="normal"
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { defaults, resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
+import { gql } from "apollo-boost";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
@@ -19,6 +20,18 @@ const client = new ApolloClient({
   resolvers,
   typeDefs
 });
+
+client
+  .query({
+    query: gql`
+      {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
