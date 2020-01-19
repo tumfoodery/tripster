95e6f5ebec3260dfb1daf9be528c64dc949f29da
Michael Yuen <michaelclaytonyuen@gmail.com>
2020-01-19 11:29:30 -0500

    trying this

--- server/src/lambda/graphql.js
+++ server/src/lambda/graphql.js
@@ -15,6 +15,8 @@ const firebaseConfig = {
   measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
+console.log({ firebaseConfig });
+
 const dataSources = () => ({
   firebaseAPI: new FirebaseAPI({ firebaseConfig })
 });
