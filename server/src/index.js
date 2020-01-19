dfbb26baa365ef6c30b2884b4a2f3f84e73ab5b7
Michael Yuen <michaelclaytonyuen@gmail.com>
2020-01-18 20:42:04 -0500

    Refactor .env, add env.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,21 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
+const env = require("./env");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const FirebaseAPI = require("./datasources/firebase");
 
+const firebaseConfig = {
+  FIREBASE_API_KEY: env.FIREBASE_API_KEY,
+  FIREBASE_DATABASE_URL: env.FIREBASE_DATABASE_URL,
+  FIREBASE_PROJECT_ID: env.FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: env.FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: env.FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: env.FIREBASE_APP_ID
+};
+
 const dataSources = () => ({
-  firebaseAPI: new FirebaseAPI({
-    firebaseConfig: JSON.parse(process.env.FIREBASE_CONFIG)
-  })
+  firebaseAPI: new FirebaseAPI({ firebaseConfig })
 });
 
 const server = new ApolloServer({
