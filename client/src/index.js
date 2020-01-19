83740880f9242d3d0df71b8277384d75dad235f8
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-19 15:40:53 -0500

    Comment out query

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { defaults, resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
-import { gql } from "apollo-boost";
+// import { gql } from "apollo-boost";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
@@ -21,17 +21,17 @@ const client = new ApolloClient({
   typeDefs
 });
 
-client
-  .query({
-    query: gql`
-      {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
+// client
+//   .query({
+//     query: gql`
+//       {
+//         rates(currency: "USD") {
+//           currency
+//         }
+//       }
+//     `
+//   })
+//   .then(result => console.log(result));
 
 ReactDOM.render(
   <ApolloProvider client={client}>
