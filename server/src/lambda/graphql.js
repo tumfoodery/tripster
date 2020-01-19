c7726e6a2c051ec089189dfc38c761282e6d967f
Michael Yuen <michaelclaytonyuen@gmail.com>
2020-01-19 11:55:36 -0500

    working functions, attempting with modified env approach

--- server/src/lambda/graphql.js
+++ server/src/lambda/graphql.js
@@ -1,21 +1,20 @@
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-lambda");
+const env = require("../env");
 const typeDefs = require("../schema");
 const resolvers = require("../resolvers");
 const FirebaseAPI = require("../datasources/firebase");
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_PROJECT_ID,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: env.FIREBASE_API_KEY,
+  authDomain: env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: env.FIREBASE_PROJECT_ID,
+  projectId: env.FIREBASE_PROJECT_ID,
+  storageBucket: env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.FIREBASE_APP_ID,
+  measurementId: env.FIREBASE_MEASUREMENT_ID
 };
-
-console.log({ firebaseConfig });
 
 const dataSources = () => ({
   firebaseAPI: new FirebaseAPI({ firebaseConfig })
