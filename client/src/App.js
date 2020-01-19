45777cb84439d80db412a4dfc46246ea9d31fc75
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-19 14:40:49 -0500

    Move apollo provider to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,20 @@
 import React from 'react';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { defaults, resolvers } from "./resolvers";
-import { typeDefs } from "./schema";
 import Login from './components/Login';
 import SignUp from './components/SignUp';
 import Copyright from './components/Copyright';
 
-const cache = new InMemoryCache();
-const client = new ApolloClient({
-  uri: 'https://tripster-apollo.netlify.com/.netlify/functions/graphql',
-  cache,
-  resolvers,
-  typeDefs
-});
-
-cache.writeData({
-  data: defaults
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Login} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/signup" component={SignUp} />
-        </Switch>
-        <Copyright />
-      </Router>
-    </ApolloProvider>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/login" exact component={Login} />
+        <Route path="/signup" component={SignUp} />
+      </Switch>
+      <Copyright />
+    </Router>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import ApolloClient, { InMemoryCache } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { defaults, resolvers } from "./resolvers";
+import { typeDefs } from "./schema";
 import * as serviceWorker from './serviceWorker';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const cache = new InMemoryCache();
+
+cache.writeData({
+  data: defaults
+});
+
+const client = new ApolloClient({
+  uri: 'https://tripster-apollo.netlify.com/.netlify/functions/graphql',
+  cache,
+  resolvers,
+  typeDefs
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>, 
+  document.getElementById('root')
+);
 
 serviceWorker.register();
