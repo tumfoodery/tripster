794c2144122fc83ec5875eca087c5fca91ab85c4
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-19 16:36:31 -0500

    Update

--- client/src/index.js
+++ client/src/index.js
@@ -4,16 +4,12 @@ import ApolloClient, { InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { defaults, resolvers } from "./resolvers";
 import { typeDefs } from "./schema";
-// import { gql } from "apollo-boost";
+import { gql } from "apollo-boost";
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-const cache = new InMemoryCache();
-
-cache.writeData({
-  data: defaults
-});
-
+const cache = new InMemoryCache().writeData({ data: defaults });
+  
 const client = new ApolloClient({
   uri: 'https://tripster-apollo.netlify.com/.netlify/functions/graphql',
   cache,
@@ -21,17 +17,17 @@ const client = new ApolloClient({
   typeDefs
 });
 
-// client
-//   .query({
-//     query: gql`
-//       {
-//         rates(currency: "USD") {
-//           currency
-//         }
-//       }
-//     `
-//   })
-//   .then(result => console.log(result));
+client
+  .query({
+    query: gql`
+      {
+        isLoggedIn
+      }
+    `
+  })
+  .then(result => {
+    console.log(result)
+  });
 
 ReactDOM.render(
   <ApolloProvider client={client}>
