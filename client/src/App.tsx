f9d2b296abb2295d73bfbc42bff13e169fe54925
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-26 18:14:51 -0500

    Notifications

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState }from 'react';
 import { Normalize } from 'styled-normalize';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Toast from 'react-toast-component';
 import Login from "./components/Login";
 import Home from "./components/Home";
 import SignUp from "./components/SignUp";
 
 const App: React.FC = () => {
+  const [isOpen, setToast] = useState(true);
   return (
     <>
+      <Toast
+        isOpen={isOpen}
+        hasAutoDismiss={false}
+        hasCloseBtn
+        closeCallback={() => setToast(false)}
+        description="There's some great info here."
+        title="App Notification!!"
+        duration={5000}
+        classNames={['info']}  // 'success', 'info', 'warning', 'error'
+      />
       <Normalize />
       <Router>
         <Switch>
