5d10ef87e339fc2f2aa48dcbba725297065ef9a4
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-26 22:15:08 -0500

    Test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Toast from "react-toast-component"; 
 import Login from "./components/Login";   
 import Home from "./components/Home";
-import SignUp from './components/SignUp'; 
+import SignUp from './components/SignUp';  
 
 const App: React.FC = () => {
   return (
