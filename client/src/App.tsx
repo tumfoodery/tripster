6425269ad7bf60e01916d1cd43d12d61d66e6fd2
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-01-26 22:07:33 -0500

    Test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Normalize } from "styled-normalize";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Toast from "react-toast-component"; 
-import Login from "./components/Login";
+import Login from "./components/Login";   
 import Home from "./components/Home";
 import SignUp from './components/SignUp';
 
