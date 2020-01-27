881464ea822499fbec270a2fe38f5ab52626a399
Michael Yuen <michaelclaytonyuen@gmail.com>
2020-01-26 21:07:58 -0500

    Refactor dataSources into separate admin and client apis, Refactor resolvers to suit, Integrate with firebase-admin to create/get/delete user in firestore

--- server/src/datasources/firebaseClient.js
+++ server/src/datasources/firebaseClient.js
@@ -1,50 +1,18 @@
-// https://stackoverflow.com/questions/41214625/firebase-node-js-error-the-xmlhttprequest-compatibility-library-was-not-foun
-global["XMLHttpRequest"] = require("xmlhttprequest").XMLHttpRequest;
 const { DataSource } = require("apollo-datasource");
+const { ApolloError, AuthenticationError } = require("apollo-server-lambda");
 
-class FirebaseAPI extends DataSource {
+class FirebaseClient extends DataSource {
   constructor({ firebase }) {
     super();
     this.firebase = firebase;
   }
 
-  /**
-   * This is a function that gets called by ApolloServer when being setup.
-   * This function gets called with the datasource config including things
-   * like caches and context. We'll assign this.context to the request context
-   * here, so we can know about the user making requests
-   */
   initialize(config) {
     this.context = config.context;
   }
 
-  async deleteUser() {
-    try {
-      if (!this.context.user) {
-        return null;
-      }
-
-      return await user.delete();
-    } catch (error) {
-      console.error(error);
-      return error.message;
-    }
-  }
-
-  getCurrentUser() {
-    if (!this.context.user) {
-      return null;
-    }
-
-    return this.context.user;
-  }
-
   async login(args) {
     try {
-      if (this.context.user) {
-        return await user.getIdToken();
-      }
-
       const { email, password } = args;
       const { user } = await this.firebase
         .auth()
@@ -54,28 +22,33 @@ class FirebaseAPI extends DataSource {
        * Don't want to interact with firebase in a stateful manner
        * i.e., token is what will determine if user is logged in, not firebase
        */
-      await this.firebase.auth().signOut();
-      return await user.getIdToken();
+      this.firebase.auth().signOut();
+      return user.getIdToken();
     } catch (error) {
       console.error(error);
-      return error.message;
+      throw new AuthenticationError(error.message);
     }
   }
 
-  async sendEmailVerification() {
+  async sendEmailVerification(customToken) {
     try {
       if (!this.context.user) {
-        return "No user is logged in";
+        throw new AuthenticationError("Operation requires authenticated user");
       }
 
       if (this.context.user.emailVerified) {
-        return "User's email is already verified";
+        throw new ApolloError("User's email is already verified");
       }
 
-      return await this.context.user.sendEmailVerification;
+      const { user } = await this.firebase
+        .auth()
+        .signInWithCustomToken(customToken);
+      await user.sendEmailVerification();
+      this.firebase.auth().signOut();
+      return;
     } catch (error) {
       console.error(error);
-      return error.message;
+      throw new ApolloError(error.message);
     }
   }
 
@@ -84,14 +57,14 @@ class FirebaseAPI extends DataSource {
       return await this.firebase.auth().sendPasswordResetEmail(email);
     } catch (error) {
       console.error(error);
-      return error.message;
+      throw new ApolloError(error.message);
     }
   }
 
   async signup(args) {
     try {
       if (this.context.user) {
-        return "Why are you trying to sign up again?";
+        throw new AuthenticationError("Why are you trying to sign up again?");
       }
 
       const { email, password } = args;
@@ -101,19 +74,18 @@ class FirebaseAPI extends DataSource {
         .auth()
         .createUserWithEmailAndPassword(email, password);
 
-      // Attempt to send email verification
-      await user.sendEmailVerification();
+      user.sendEmailVerification();
 
       /**
        * Immediately logout
        * Don't want to interact with firebase in a stateful manner
        * i.e., token is what will determine if user is logged in, not firebase
        */
-      await this.firebase.auth().signOut();
-      return await user.getIdToken();
+      this.firebase.auth().signOut();
+      return user;
     } catch (error) {
       console.error(error);
-      return error.message;
+      throw new AuthenticationError(error.message);
     }
   }
 
@@ -122,9 +94,9 @@ class FirebaseAPI extends DataSource {
       return await this.firebase.auth().applyActionCode(code);
     } catch (error) {
       console.error(error);
-      return error.message;
+      throw new ApolloError(error.message);
     }
   }
 }
 
-module.exports = FirebaseAPI;
+module.exports = FirebaseClient;
