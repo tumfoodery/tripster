d1bc9af985a1bdb5829e651257dbc4d317af9c03
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-02-02 00:36:04 -0500

    Update client schema

--- client/src/schema/index.ts
+++ client/src/schema/index.ts
@@ -1,30 +1,3 @@
 import gql from "graphql-tag";
 
-export const typeDefs = gql`
-  type Mutation {
-    confirmPasswordReset(code: String!, newPassword: String!): String
-    deleteUser: String
-    login(email: String!, password: String!): String
-    signup(
-      email: String!
-      firstName: String!
-      lastName: String!
-      password: String!
-    ): String
-    verifyEmail(code: String!): String
-    verifyPasswordResetCode(code: String!): String
-  }
-
-  type Query {
-    currentUser: User
-    sendEmailVerification: String
-    sendPasswordResetEmail(email: String!): String
-  }
-
-  type User {
-    email: String!
-    emailVerified: Boolean!
-    firstName: String!
-    lastName: String!
-  }
-`;
+export const typeDefs = gql``;
