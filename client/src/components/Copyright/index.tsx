e5e5ced2239973cc327f03727584048a3b785919
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-02-01 22:53:31 -0500

    Centering

--- client/src/components/Copyright/index.tsx
+++ client/src/components/Copyright/index.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 const CopyrightContainer = styled.footer`
   font-size: ${props => props.theme.font.small};
+  text-align: center;
 `;
 
 export default function Copyright() {
