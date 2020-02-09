c61d2b68f42d42959510bbc693b7dcb6ce1bd39e
Sue Ann Ioanis <sue.ioanis@leaflink.com>
2020-02-09 10:52:23 -0400

    Trip

---
+++ client/src/components/Trip/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const TripContainer = styled.div``;
+
+export default function Trip() {
+  return <TripContainer>Trip</TripContainer>;
+}
